ee019d31171ee0ee17c8d40c4a49bc412552fb24
RagnarSmari <raggism98@gmail.com>
2021-11-23 21:53:42 +0000

    test

---
+++ src/views/CreateBoard/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const CreateBoard = function () {
+  return (
+    <Text>Here comes the input to create a new board</Text>
+  );
+};
+export default CreateBoard;
