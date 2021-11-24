37a26e79ac6ce2e94ed632fe451a27ca85d864ef
Sigrún júnía <sigrunjunia@Sigruns-MacBook-Pro.local>
2021-11-24 16:33:49 +0000

    added create board layout

---
+++ src/views/CreateBoard/styles.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  input: {
+    flexDirection: 'row',
+    height: 40,
+    alignItems: 'center',
+    margin: 12,
+    borderWidth: 1,
+    borderRadius: 8,
+    padding: 10,
+
+  },
+  addImageBtn: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 80,
+  },
+
+  text: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 12,
+  },
+});
