8970478d0ae6317b9dfa77c4092553ad16b90f8b
Sigrún júnía <sigrunjunia@Sigruns-MacBook-Pro.local>
2021-11-26 22:34:49 +0000

    styling

--- src/components/ListAllLists/styles.js
+++ src/components/ListAllLists/styles.js
@@ -1,4 +1,5 @@
 import { StyleSheet } from 'react-native';
+import { white, gray } from '../../styles';
 
 export default StyleSheet.create({
   textStyle: {
@@ -9,9 +10,42 @@ export default StyleSheet.create({
     marginTop: 10,
 
   },
+
+  input: {
+    flexDirection: 'row',
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    borderRadius: 8,
+    padding: 10,
+  },
+
   ModalContainerCreateList: {
     backgroundColor: 'white',
-    width: '100%',
-    height: '100%',
+    borderRadius: 20,
+
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'black',
+  },
+
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+  optionText: {
+    flexDirection: 'row',
+    textAlign: 'right',
+    margin: 10,
+    color: 'black',
   },
 });
